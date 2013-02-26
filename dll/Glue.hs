2d2e981ae3afff75e3c2246526a80dd1decf091b
CK Kashyap <ckkashyap@gmail.com>
2013-02-26 15:41:43 +0530

    dll works

--- dll/Glue.hs
+++ dll/Glue.hs
@@ -2,10 +2,10 @@
 module Glue where
 import Network.HSGmail
 
-
-
 adder :: Int -> Int -> IO Int  -- gratuitous use of IO
-adder x y = return (bingo x y)
+adder x y = do
+      dummy
+      return (x+y)
 
 
 foreign export stdcall adder :: Int -> Int -> IO Int
--- dll/build.bat
+++ dll/build.bat
@@ -1,6 +1,5 @@
-copy C:\Users\kck\AppData\Roaming\cabal\hs-gmail-0.1.0.0\ghc-7.4.2\libHShs-gmail*.a
 ghc -c Glue.hs
 ghc -c StartEnd.c
-ghc -shared -o Glue.dll Glue.o StartEnd.o libHShs-gmail-0.1.0.0.a
+ghc -shared -o Glue.dll Glue.hs StartEnd.o
 runhaskell filter.hs Glue_stub.h stdcall
 cl /I"C:\Program Files (x86)\Haskell Platform\2012.4.0.0\lib\include" haskell_from_cpp.cpp
